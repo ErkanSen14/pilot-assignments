04970fc198fe396b4f457ed06c8b59c5a701b853
Erkan Sen <erkansen14@hotmail.com>
2016-08-30 18:04:47 -0500

    added solutions for 2 and 3

--- week1/day1/2.js
+++ week1/day1/2.js
@@ -1,5 +1,17 @@
 var console = require('./lib/consolelog.js');
 
 module.exports = function fizzBuzz() {
-  // write your code here
+  for (var num = 1; num < 101; num++) {
+    if (num % 3 === 0) {
+    	if (num % 5 === 0) {
+    		console.log("FizzBuzz")
+    	} else {
+    	    console.log("Fizz");
+    	}
+    } else if (num % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(num);
+    }
+  }
 }
--- week1/day1/3.js
+++ week1/day1/3.js
@@ -1,5 +1,26 @@
 var console = require('./lib/consolelog.js');
 
 module.exports = function chessboard() {
-  // write your code here
+  var size = 8;
+  var x = '# ';
+  var y = ' #';
+  var tempOutput1 = '';
+  var tempOutput2 = '';
+  var output = '';
+  for (var j = 0; j < size / 2; j++) {
+    tempOutput1 += x;
+    tempOutput2 += y;
+  }
+  for (var i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      output += tempOutput1 + '\n';
+    }
+    else if (i === (size - 1)) {
+      output += tempOutput2;
+    }
+    else {
+      output += tempOutput2 + '\n';
+    }
+  }
+  console.log(output)
 }
