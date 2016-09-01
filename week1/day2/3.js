26e62f214f42054f16466131fd7cb782bd04330d
Erkan Sen <erkansen14@hotmail.com>
2016-09-01 15:54:21 -0500

    fixed recursion

--- week1/day2/3.js
+++ week1/day2/3.js
@@ -1,6 +1,9 @@
 module.exports = function prime(x, counter) {
-  if (x === 0 || x === 1 || x < 0 || x === 2) {
+  if (x === 0 || x === 1 || x < 0) {
     return false;
+  }
+  if (x === 2) {
+    return true;
   }
   if (counter === x) {
     return;
@@ -8,7 +11,11 @@ module.exports = function prime(x, counter) {
   if (x % counter === 0) {
     return false;
   } else {
-    prime(x, counter + 1);
+    var temp = prime(x, counter + 1);
+    if (temp === undefined) {
+    	return true;
+    } else {
+    	return temp;
+    }
   }
-  return true;
 }
